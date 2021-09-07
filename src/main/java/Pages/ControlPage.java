736d62aea1ba1da986b0b316a1ca93dbbd6229b1
DankovAlexander <dankoffalexander@gmail.com>
2021-09-07 23:10:48 +0500

    Добавлены локаторы калькулятора

--- src/main/java/Pages/ControlPage.java
+++ src/main/java/Pages/ControlPage.java
@@ -1,4 +1,58 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.support.PageFactory.*;
+
 public class ControlPage {
+
+    @FindBy(css = "div[jsname='j93WEe']")
+    public WebElement openBracket;
+
+    @FindBy(css = "div[jsname='N10B9']")
+    public WebElement one;
+
+    @FindBy(css = "div[jsname='XSr6wc']")
+    public WebElement plus;
+
+    @FindBy(css = "div[jsname='qCp9A']")
+    public WebElement closeBracket;
+
+    @FindBy(css = "div[jsname='YovRWb']")
+    public WebElement multiply;
+
+    @FindBy(css = "div[jsname='KN1kY']")
+    public WebElement three;
+
+    @FindBy(css = "div[jsname='pPHzQc']")
+    public WebElement minus;
+
+    @FindBy(css = "div[jsname='xAP7E']")
+    public WebElement four;
+
+    @FindBy(css = "div[jsname='bkEvMb']")
+    public WebElement zero;
+
+    @FindBy(css = "div[jsname='WxTTNd']")
+    public WebElement divide;
+
+    @FindBy(css = "div[jsname='Ax5wH']")
+    public WebElement five;
+
+    @FindBy(css = "div[jsname='Pt8tGc']")
+    public WebElement equals;
+
+    @FindBy(css = "div[jsname='abcgof']")
+    public WebElement six;
+
+    @FindBy(css = "div[jsname='aN1RFf']")
+    public WebElement sinus;
+
+
+    public ControlPage(WebDriver driver) {
+        initElements(driver, this);
+    }
 }
