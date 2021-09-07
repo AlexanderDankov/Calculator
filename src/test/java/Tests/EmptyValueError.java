341c8dcdaf4473ee15037b854d2297fab7cf340b
DankovAlexander <dankoffalexander@gmail.com>
2021-09-08 00:52:16 +0500

    Написан тест-кейс проверки ошибки при отсутствии значения

---
+++ src/test/java/Tests/EmptyValueError.java
@@ -0,0 +1,47 @@
+package Tests;
+
+import Pages.ControlPage;
+import Pages.SearchPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmptyValueError {
+    private static WebDriver driver;
+    private static SearchPage searchPage;
+    private static ControlPage controlPage;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        searchPage = new SearchPage(driver);
+        controlPage = new ControlPage(driver);
+    }
+    @Test
+    @DisplayName("Проверка ошибки при отсутствии значения")
+    public void test1() {
+
+        driver.get("https://www.google.com/");
+        searchPage.search("Калькулятор");
+
+        controlPage.sinus.click();
+        controlPage.equals.click();
+
+        assertEquals("Error", controlPage.result.getText());
+    }
+
+    @AfterAll
+    public static void teardown() throws InterruptedException {
+        Thread.sleep(4000L);
+        driver.quit();
+    }
+}
